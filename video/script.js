b910c9c45f0a785edd548c51acd6b159686f3803
btom13 <brianatom@gmail.com>
2023-04-19 23:26:26 -0700

    blockbuster

--- video/script.js
+++ video/script.js
@@ -6,10 +6,10 @@
   const OLL = document.querySelector("#OLL");
   const PLL = document.querySelector("#PLL");
   const vid = document.querySelector("#video");
-  let times = [0, 2440, 12260, 14210, 17090];
+  let times = [0, 2.44, 12.26, 14.21, 17.09];
   let objs = [Cross, F2L, OLL, PLL];
-  vid.ontimeupdate = (l) => {
-    let time = l.timeStamp;
+  vid.ontimeupdate = (event) => {
+    let time = event.target.currentTime;
     for (let i = 0; i < objs.length; i++) {
       if (times[i] < time && time < times[i + 1]) {
         objs[i].classList.add("show");
@@ -18,12 +18,29 @@
       }
     }
   };
-  vid.addEventListener("ended", () => {});
-  restart.addEventListener("click", function () {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
+  for (let i = 0; i < objs.length; i++) {
+    objs[i].addEventListener("click", () => {
+      restart.style.display = "none";
+      vid.currentTime = times[i];
+      vid.play();
+    });
+  }
+
+  // restart.sty("display", "block");
+  // console.log((restart.style.display = "block"));
+  restart.addEventListener("click", () => {
+    restart.style.display = "none";
+    vid.currentTime = 0;
+    vid.play();
   });
+  vid.addEventListener("ended", () => {
+    restart.style.display = "block";
+  });
+  // restart.addEventListener("click", function () {
+  //   if (!document.fullscreenElement) {
+  //     document.documentElement.requestFullscreen();
+  //   } else {
+  //     document.exitFullscreen();
+  //   }
+  // });
 })();
